160de54eab61a6b2450b58d297c187f70c100c51
Jithu2pixelo <jithu.jacob@pixelotech.com>
2024-02-13 00:35:19 +0530

    quatation page desings generated

--- cochinhotelequip/quotation/urls.py
+++ cochinhotelequip/quotation/urls.py
@@ -4,5 +4,6 @@ from quotation.views.quotation import *
 
 urlpatterns = [
     path('', quotation, name='quotation'),
-    # path('delete_product/<int:product_id>', delete_product, name='delete_product'),
+    path('select_customer', select_customer, name='select_customer'),
+    path('generate_quotation/<int:customer_id>', generate_quotation, name='generate_quotation'),
 ]
--- cochinhotelequip/quotation/views/quotation.py
+++ cochinhotelequip/quotation/views/quotation.py
@@ -6,6 +6,9 @@ from django.db.models import Q
 from django.db import transaction
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 import re
+from account.models.user import User
+from account.models.address import Address
+from product.models.product import Product
 
 
 @login_required(login_url='login')
@@ -35,4 +38,67 @@ def quotation(request):
 
     context = {"quotation_list":quotation_list, "active_page":"quotation"}
     return render(request, 'quotation/quotation.html', context)
+
+
+@login_required(login_url='login')
+def select_customer(request):
+    if request.method == 'GET':
+        customers = User.objects.all().order_by("-date_joined")
+    else:
+        search = request.POST.get('search')
+        q_object = Q()
+        q_object.add(Q(first_name__icontains=search), Q.OR)
+        q_object.add(Q(last_name__icontains=search), Q.OR)
+        q_object.add(Q(phone__icontains=search), Q.OR)
+
+        customers = User.objects.filter(q_object).order_by("-date_joined")
     
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(customers, 2)
+    try:
+        customers = paginator.page(page)
+    except PageNotAnInteger:
+        customers = paginator.page(1)
+    except EmptyPage:
+        customers = paginator.page(paginator.num_pages)
+
+    context = {"customers": customers, "active_page":"quotation"}
+    return render(request, 'quotation/select_customer.html', context)
+
+
+@login_required(login_url='login')
+def generate_quotation(request, customer_id):
+    customer = User.objects.get(id=customer_id)
+    address = Address.objects.filter(customer=customer).order_by('is_default')
+    primary_address = address[0]
+
+    if request.method == 'GET':
+        product = Product.objects.all().order_by('-created_at')
+    else:
+        search = request.POST.get('search')
+        q_object = Q()
+        # q_object.add(Q(first_name__icontains=search), Q.OR)
+        # q_object.add(Q(last_name__icontains=search), Q.OR)
+        # q_object.add(Q(phone__icontains=search), Q.OR)
+
+        product = Product.objects.filter(q_object).order_by('-created_at')
+    
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(product, 2)
+    try:
+        product = paginator.page(page)
+    except PageNotAnInteger:
+        product = paginator.page(1)
+    except EmptyPage:
+        product = paginator.page(paginator.num_pages)
+
+    context = {
+        "product": product, 
+        "active_page":"quotation",
+        "customer":customer,
+        "address":address,
+        "primary_address":primary_address
+        }
+    return render(request, 'quotation/generate_quotation.html', context)
